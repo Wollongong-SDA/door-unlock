d9378531c4367b6ab40f7e6a9d5b0d5461ca046c
Neeron Bhatta <33238007+NeuronButter@users.noreply.github.com>
2024-01-27 16:32:08 +1100

    Better logs + Separated Unifi commands

--- index.js
+++ index.js
@@ -1,130 +1,54 @@
-// import fetch from "node-fetch";
-import {fetch, CookieJar} from "node-fetch-cookies";
-import https from 'https';
-import dotenv from 'dotenv' ;
+import dotenv from 'dotenv';
 import express from 'express';
+import {leaveUnlocked, lock, login} from './unifi.js'
+import { consola, createConsola } from "consola";
+
 dotenv.config()
-
-const httpAgent = new https.Agent({
-  rejectUnauthorized: false
-})
-
-const cookieJar = new CookieJar('./CookieJar.json');
-let token;
-
-let lastLogin = new Date("1970-01-01");
-
-const login = async () => {
-  const username = process.env.USER;
-  const password = process.env.PASS;
-  const path = "https://10.2.100.1/api/auth/login";
-
-  if ((new Date() - lastLogin) / (1000 * 60) <= 30) {
-    console.info("Skipping (already logged in)")
-    return
-  }
-
-  const response = await fetch(cookieJar, path, {
-    method: 'POST',
-    headers: {
-      'accept': 'application/json',
-      'content-type': 'application/json',
-    },
-    body: JSON.stringify({
-      username: username,
-      password: password,
-      remember: true
-    }),
-    agent: httpAgent
-  })
-
-  if (response.ok) {
-    console.log("Login Ok")
-    const data = await response.json();
-    console.log(data);
-    // console.log(response.headers);
-    lastLogin = Date.now()
-    token = response.headers.get('x-csrf-token')
-  } else {
-    console.log("Login Failed")
-    console.log(response)
-  }
-
-}
-
-const leaveUnlocked = async () => {
-  const response = await fetch(cookieJar, "https://10.2.100.1/proxy/access/api/v2/device/d8b3702b4d56/lock_rule", {
-    "headers": {
-      "accept": "application/json, text/plain, */*",
-      "accept-language": "en-AU,en",
-      "content-type": "application/json",
-      "x-csrf-token": token
-    },
-    "referrerPolicy": "no-referrer",
-    "body": "{\"type\":\"keep_unlock\"}",
-    "method": "PUT",
-    "mode": "cors",
-    "credentials": "include",
-    agent: httpAgent
-  });
-
-  if (response.ok) {
-    console.log("Leave Unlocked Ok")
-    const data = await response.json();
-    console.log(data);
-  } else {
-    console.log("Leave Unlocked Failed")
-    const data = await response.headers;
-    console.log(data);
-  }
-}
-
-const lock = async () => {
-  const response = await fetch(cookieJar, "https://10.2.100.1/proxy/access/api/v2/device/d8b3702b4d56/lock_rule", {
-    "headers": {
-      "accept": "application/json, text/plain, */*",
-      "accept-language": "en-AU,en",
-      "content-type": "application/json",
-      "x-csrf-token": token
-    },
-    "referrerPolicy": "no-referrer",
-    "body": "{\"type\":\"reset\"}",
-    "method": "PUT",
-    "mode": "cors",
-    "credentials": "include",
-    agent: httpAgent
-  });
-
-  if (response.ok) {
-    console.log("Relock Ok")
-    const data = await response.json();
-    console.log(data);
-  } else {
-    console.log("Relock Failed")
-    console.log(response)
-  }
-}
+let lastTrigger = new Date('1970-01-01')
 
 const app = express()
+
+// Whitelisting
+app.use((req, res, next) => {
+  if (!req.ip.includes("10.2.140.2")) {
+    consola.warn(`Unauthorized IP ${req.ip} requested '${req.path}'!`)
+    return res.sendStatus(403)
+  }
+
+  next()
+})
+
 app.get('/leaveUnlocked', async (req, res) => {
-  if (!req.ip.includes("10.2.140.2")) {
-    return res.sendStatus(403);
+  const loginRes = await login();
+  if (!loginRes.success) {
+    consola.error(new Error("Login Failed"), loginRes.body)
+    res.sendStatus(500)
   }
 
-  await login();
-  await leaveUnlocked();
+  const unlockRes = await leaveUnlocked();
+  if (!unlockRes.success) {
+    consola.error(new Error("Unlock Failed"), unlockRes.body)
+    res.sendStatus(500)
+  }
 
   res.sendStatus(200);
 })
+
 app.get('/relock', async (req, res) => {
-  if (!req.ip.includes("10.2.140.2")) {
-    return res.sendStatus(403);
+  const loginRes = await login();
+  if (!loginRes.success) {
+    consola.error(new Error("Login Failed"), loginRes.body)
+    res.sendStatus(500)
   }
 
-  await login();
-  await lock();
+  const lockRes = await lock();
+  if (!lockRes.success) {
+    consola.error(new Error("Relock failed"), loginRes.body)
+    res.sendStatus(500)
+  }
 
   res.sendStatus(200);
 })
 
-app.listen(3000, () => console.log('Started listening'))
+const port = process.env.PORT || 3000
+app.listen(port, () => consola.success(`Express started on port ${port}`))
---
+++ unifi.js
@@ -0,0 +1,92 @@
+import https from 'https'
+import {fetch, CookieJar} from "node-fetch-cookies"
+import { consola, createConsola } from 'consola'
+
+let token
+let lastLogin = new Date('1970-01-01')
+
+const cookieJar = new CookieJar('./CookieJar.json')
+
+const httpAgent = new https.Agent({ rejectUnauthorized: false })
+
+export const login = async () => {
+  if ((new Date() - lastLogin) / (1000 * 60) <= 30) {
+    console.info('Skipping (already logged in)')
+    return {success: true, body: {}}
+  }
+
+  const response = await fetch(cookieJar, 'https://10.2.100.1/api/auth/login', {
+    method: 'POST',
+    headers: {
+      accept: 'application/json',
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify({
+      username: process.env.USER,
+      password: process.env.PASS,
+      remember: true,
+    }),
+    agent: httpAgent,
+  })
+
+  if (response.ok) {
+    lastLogin = Date.now()
+    token = response.headers.get('x-csrf-token')
+    const data = response.json()
+    return {success: true, body: data}
+  }
+
+  return {success: false, body: response};
+}
+
+export const leaveUnlocked = async () => {
+  const response = await fetch(cookieJar, 'https://10.2.100.1/proxy/access/api/v2/device/d8b3702b4d56/lock_rule', {
+    headers: {
+      accept: 'application/json, text/plain, */*',
+      'accept-language': 'en-AU,en',
+      'content-type': 'application/json',
+      'x-csrf-token': token,
+    },
+    referrerPolicy: 'no-referrer',
+    body: '{"type":"keep_unlock"}',
+    method: 'PUT',
+    mode: 'cors',
+    credentials: 'include',
+    agent: httpAgent,
+  })
+
+  if (response.ok) {
+    lastLogin = Date.now()
+    token = response.headers.get('x-csrf-token')
+    const data = response.json()
+    return {success: true, body: data}
+  }
+
+  return {success: false, body: response};
+}
+
+export const lock = async () => {
+  const response = await fetch(cookieJar, 'https://10.2.100.1/proxy/access/api/v2/device/d8b3702b4d56/lock_rule', {
+    headers: {
+      accept: 'application/json, text/plain, */*',
+      'accept-language': 'en-AU,en',
+      'content-type': 'application/json',
+      'x-csrf-token': token,
+    },
+    referrerPolicy: 'no-referrer',
+    body: '{"type":"reset"}',
+    method: 'PUT',
+    mode: 'cors',
+    credentials: 'include',
+    agent: httpAgent,
+  })
+
+  if (response.ok) {
+    lastLogin = Date.now()
+    token = response.headers.get('x-csrf-token')
+    const data = response.json()
+    return {success: true, body: data}
+  }
+
+  return {success: false, body: response};
+}
