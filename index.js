7153f47513b5ba31e78ab115bc4417fedddd7ac7
Neeron Bhatta <33238007+NeuronButter@users.noreply.github.com>
2024-01-27 16:36:07 +1100

    Reject all clause + formatting

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import dotenv from 'dotenv';
-import express from 'express';
-import {leaveUnlocked, lock, login} from './unifi.js'
-import { consola, createConsola } from "consola";
+import dotenv from 'dotenv'
+import express from 'express'
+import { leaveUnlocked, lock, login } from './unifi.js'
+import { consola } from 'consola'
 
 dotenv.config()
 let lastTrigger = new Date('1970-01-01')
@@ -10,44 +10,48 @@ const app = express()
 
 // Whitelisting
 app.use((req, res, next) => {
-  if (!req.ip.includes("10.2.140.2")) {
+  if (!req.ip.includes('10.2.140.2')) {
     consola.warn(`Unauthorized IP ${req.ip} requested '${req.path}'!`)
     return res.sendStatus(403)
+  }
+
+  if (process.env.REJECTALL == 'true') {
+    return res.sendStatus(500)
   }
 
   next()
 })
 
 app.get('/leaveUnlocked', async (req, res) => {
-  const loginRes = await login();
+  const loginRes = await login()
   if (!loginRes.success) {
-    consola.error(new Error("Login Failed"), loginRes.body)
+    consola.error(new Error('Login Failed'), loginRes.body)
     res.sendStatus(500)
   }
 
-  const unlockRes = await leaveUnlocked();
+  const unlockRes = await leaveUnlocked()
   if (!unlockRes.success) {
-    consola.error(new Error("Unlock Failed"), unlockRes.body)
+    consola.error(new Error('Unlock Failed'), unlockRes.body)
     res.sendStatus(500)
   }
 
-  res.sendStatus(200);
+  res.sendStatus(200)
 })
 
 app.get('/relock', async (req, res) => {
-  const loginRes = await login();
+  const loginRes = await login()
   if (!loginRes.success) {
-    consola.error(new Error("Login Failed"), loginRes.body)
+    consola.error(new Error('Login Failed'), loginRes.body)
     res.sendStatus(500)
   }
 
-  const lockRes = await lock();
+  const lockRes = await lock()
   if (!lockRes.success) {
-    consola.error(new Error("Relock failed"), loginRes.body)
+    consola.error(new Error('Relock failed'), loginRes.body)
     res.sendStatus(500)
   }
 
-  res.sendStatus(200);
+  res.sendStatus(200)
 })
 
 const port = process.env.PORT || 3000
