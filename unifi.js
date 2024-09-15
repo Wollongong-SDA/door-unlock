31880a963d5962fce5cdc3708e42fe602f1115a2
Neeron Bhatta <33238007+NeuronButter@users.noreply.github.com>
2024-09-15 12:18:55 +1000

    Cleanup

--- unifi.js
+++ unifi.js
@@ -1,20 +1,19 @@
 import https from 'https'
 import fetch from "node-fetch"
-import { consola } from 'consola'
 
 import dotenv from 'dotenv'
 dotenv.config()
 
 const httpAgent = new https.Agent({ rejectUnauthorized: false })
 
-export const leaveUnlocked = async () => {
+const request = async (body) => {
   const response = await fetch(`https://${process.env.HOST}/api/v1/developer/doors/${process.env.DOOR}/lock_rule`, {
     headers: {
       accept: 'application/json',
       'content-type': 'application/json',
       'Authorization': `Bearer ${process.env.API_KEY}`
     },
-    body: '{"type":"keep_unlock"}',
+    body: body,
     method: 'PUT',
     agent: httpAgent,
   })
@@ -27,22 +26,10 @@ export const leaveUnlocked = async () => {
   return {success: false, body: response};
 }
 
+export const leaveUnlocked = async () => {
+  return request('{"type":"keep_unlock"}')
+}
+
 export const lock = async () => {
-  const response = await fetch(`https://${process.env.HOST}/api/v1/developer/doors/${process.env.DOOR}/lock_rule`, {
-    headers: {
-      accept: 'application/json',
-      'content-type': 'application/json',
-      'Authorization': `Bearer ${process.env.API_KEY}`
-    },
-    body: '{"type":"reset"}',
-    method: 'PUT',
-    agent: httpAgent,
-  })
-
-  if (response.ok) {
-    const data = await response.json()
-    return {success: true, body: data}
-  }
-
-  return {success: false, body: response};
+  return request('{"type":"reset"}')
 }
